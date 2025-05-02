52e19a3b88104e1dcba8df8f52f205b80d8dd96c
aislxflames <proarghaop@gmail.com>
2025-05-02 23:37:52 +0530

    ✨ Add feature

--- dotfiles/.config/waybar/scripts/keybindings.sh
+++ dotfiles/.config/waybar/scripts/keybindings.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
-#  _              _     _           _ _
-# | | _____ _   _| |__ (_)_ __   __| (_)_ __   __ _ ___
-# | |/ / _ \ | | | '_ \| | '_ \ / _` | | '_ \ / _` / __|
-# |   <  __/ |_| | |_) | | | | | (_| | | | | | (_| \__ \
-# |_|\_\___|\__, |_.__/|_|_| |_|\__,_|_|_| |_|\__, |___/
-#           |___/                             |___/
 
-# -----------------------------------------------------
-# Use actual path to keybindings config file
-# -----------------------------------------------------
 config_file="$HOME/.config/hypr/conf/keybinds.conf"
+mainModSymbol="SUPER"
+legend="SUPER = WINDOWS KEY"
 
-# -----------------------------------------------------
-# Show where we are reading from
-# -----------------------------------------------------
-echo "reading from: $config_file"
+# Icon picker based on action/key
+get_icon() {
+    case "$1" in
+        *RaiseVolume*) echo "🔊" ;;
+        *LowerVolume*) echo "🔉" ;;
+        *Mute*) echo "🔇" ;;
+        *killactive*) echo "" ;;          # Font Awesome 'times-circle'
+        *exit*) echo "襤" ;;               # MDI 'exit-to-app'
+        *pseudo*|*togglesplit*) echo "" ;; # Material 'layout'
+        *togglefloating*) echo "" ;;      # Font Awesome 'window-restore'
+        *terminal*|*foot*|*alacritty*) echo "" ;; # Devicon 'terminal'
+        *fileManager*|*thunar*|*nautilus*) echo "" ;; # Font Awesome 'folder'
+        *launcher*|*wofi*|*rofi*) echo "" ;; # Font Awesome 'rocket'
+        *powermenu*) echo "⏻" ;;           # Unicode power icon
+        *switcher*) echo "" ;;            # Font Awesome 'desktop'
+        *) echo "󰋼" ;;                    # Default: Material 'dots-horizontal'
+    esac
+}
 
-# Parse binds: bind, bindel, bindm, etc.
-keybinds=$(grep -E '^bind' "$config_file" | while IFS=',' read -r type mod key action rest; do
-    # Replace $mainMod with SUPER
-    mod=${mod//\$mainMod/SUPER}
-    key=${key// /}
-    action=${action// /}
-    cmd="${action} ${rest}"
-    printf "%-25s → %s\n" "$mod + $key" "$cmd"
+# Parse and format keybinds
+keybinds=$(grep -E '^bind' "$config_file" | while read -r line; do
+    line="${line//\$mainMod/$mainModSymbol}"
+    line=$(echo "$line" | sed -E 's/^bind[a-z]*[[:space:]]*=[[:space:]]*//')
+
+    IFS=',' read -r mod key action rest <<< "$line"
+
+    mod=$(echo "$mod" | xargs | sed 's/+ */ + /g')
+    key=$(echo "$key" | xargs)
+    action=$(echo "$action" | xargs)
+    rest=$(echo "$rest" | xargs)
+    cmd=$(echo "$action $rest" | sed 's/^exec //')
+
+    icon=$(get_icon "$cmd")
+
+    # Only add "+" if there's a modifier
+    if [[ -n "$mod" ]]; then
+        combo="$mod + $key"
+    else
+        combo="$key"
+    fi
+
+    printf "%s  %-30s → %s\n" "$icon" "$combo" "$cmd"
 done)
 
 # Show in rofi
-sleep 0.2
-echo "$keybinds" | rofi -dmenu -i -markup -eh 2 -replace -p "keybinds"
+output="$legend\n\n$keybinds"
+echo -e "$output" | rofi -dmenu -i -markup -eh 2 -p "󰌌 Keybinds"
+
