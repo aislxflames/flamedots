ec3c1b361ece501ae4a9c3a70c69721100b56633
aislxflames <proarghaop@gmail.com>
2026-02-05 20:57:18 +0530

    🎨 Refactor config styling

--- dotfiles/.config/waybar/launch.sh
+++ dotfiles/.config/waybar/launch.sh
@@ -6,9 +6,5 @@ theme=$(cat ~/.config/waybar/theme.sh)
 # Set the FILEPATH variable
 FILEPATH="$HOME/.config/waybar/themes/$theme"
 
-# Run Waybar with the specified config and style
-if pgrep -x "waybar" > /dev/null; then
     killall waybar
-else
-    waybar -c "$FILEPATH/config" -s "$FILEPATH/style.css" & disown
-fi
+    waybar -c "$FILEPATH/config.jsonc" -s "$FILEPATH/style.css" & disown
